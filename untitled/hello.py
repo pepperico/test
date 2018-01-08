fa83d2e185fd32b4869a98beb869234c80ad7d5d
pepperico <peppe.drs.2810@gmail.com>
2018-01-08 21:52:42 +0900

    Django tutorial

--- untitled/hello.py
+++ untitled/hello.py
@@ -1,3 +1,9 @@
 a = "love"
 print("all you need is" , a)
 print(type(a))
+
+b = ''
+b += 'Yellow'
+b += '\n'
+b += 'Submarine'
+print(b)
---
+++ untitled/lecture02.py
@@ -0,0 +1,29 @@
+# 2-1
+
+hour = 60
+minute = 60
+second = hour * minute
+
+print(second)
+
+# 2-2
+
+seconds_per_hour = second
+
+# 2-3
+
+day = seconds_per_hour * 24
+print(day)
+
+# 2-4
+
+seconds_per_day = seconds_per_hour * 24
+print("seconds_per_day is " , seconds_per_day)
+
+# 2-5
+
+print(seconds_per_day / seconds_per_hour)
+
+# 2-6
+
+print(seconds_per_day // seconds_per_hour)
