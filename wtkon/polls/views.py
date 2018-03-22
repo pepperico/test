f871f8ac3f9bc232119adf88e6e893399cc303a5
pepperico <peppe.drs.2810@gmail.com>
2018-03-22 17:36:08 +0900

    Django tutorial 03 (completed)

--- wtkon/polls/views.py
+++ wtkon/polls/views.py
@@ -1,23 +1,24 @@
 from django.http import HttpResponse
-from django.template import loader  # section 3 - 3
+from django.shortcuts import get_object_or_404, render  # from django.template import loader  # section 3 - 3
 
 from .models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
     context = {'latest_question_list': latest_question_list, }
     # output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(template.render(context, request))    # "Hello, world. You're at the polls index."
+    return render(request, 'polls/index.html', context)
+    # return HttpResponse(template.render(context, request))
 
-    # Leave the rest of the views (detail, results, vote) unchanged
 
 # section 3 - 1
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
